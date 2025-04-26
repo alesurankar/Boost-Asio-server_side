8476794690271e2ae594740283a211f9ebd0a42e
ales2026 <ale.urankar@gmail.com>
2025-04-26 06:35:58 +0200

    testing boost::brast and it works

--- server_side/server.cpp
+++ server_side/server.cpp
@@ -1,4 +1,5 @@
 #include <boost/asio.hpp>
+#include <boost/beast.hpp>  // <-- Add Boost.Beast header
 #include <iostream>
 #include <thread>
 #include <atomic>
@@ -11,7 +12,7 @@ using boost::asio::ip::tcp;
 std::atomic<int> client_counter{ 0 };
 std::vector<std::shared_ptr<tcp::socket>> clients;
 std::mutex clients_mutex;
-std::map<std::shared_ptr<tcp::socket>, std::string> client_names;  // To store client names
+std::map<std::shared_ptr<tcp::socket>, std::string> client_names;
 
 // 3: Function to broadcast a message to all clients except the sender
 void broadcast(const std::string& msg, std::shared_ptr<tcp::socket> sender) {
@@ -25,6 +26,13 @@ void broadcast(const std::string& msg, std::shared_ptr<tcp::socket> sender) {
 }
 
 int main() {
+    // Adding a simple use of Boost.Beast's HTTP functionality
+    boost::beast::http::request<boost::beast::http::string_body> req{ boost::beast::http::verb::get, "/", 11 };
+    req.set(boost::beast::http::field::host, "localhost");
+    req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    std::cout << "Using Boost.Beast version: " << BOOST_BEAST_VERSION_STRING << std::endl;
+
     boost::asio::io_context io;
     tcp::acceptor acceptor(io, tcp::endpoint(boost::asio::ip::make_address("127.0.0.1"), 1234));
 
