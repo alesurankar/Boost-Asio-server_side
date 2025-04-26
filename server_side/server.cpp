4c76be006ebdbf76d78044308fb98b72bf63b71e
ales2026 <ale.urankar@gmail.com>
2025-04-26 19:54:06 +0200

    improvements on both client-server

--- server_side/server.cpp
+++ server_side/server.cpp
@@ -1,5 +1,6 @@
 #include <boost/asio.hpp>
-#include <boost/beast.hpp>  // <-- Add Boost.Beast header
+#include <boost/beast.hpp>
+#include <boost/json.hpp>
 #include <iostream>
 #include <thread>
 #include <atomic>
@@ -28,9 +29,8 @@ void broadcast(const std::string& msg, std::shared_ptr<tcp::socket> sender) {
 }
 
 // Function to send a basic signal to FastAPI server (GET request)
-void send_signal_to_fastapi(const std::string& username) {
+bool send_signal_to_fastapi(const std::string& username) {
     try {
-        // The I/O context
         boost::asio::io_context ioc;
 
         // Resolver to convert the server address and port
@@ -40,43 +40,53 @@ void send_signal_to_fastapi(const std::string& username) {
         auto const results = resolver.resolve("127.0.0.1", "8000");
 
         // Create the socket
-        boost::asio::ip::tcp::socket socket(ioc);
+        boost::asio::ip::tcp::socket fastapi_socket(ioc);
 
         // Connect to the FastAPI server
-        boost::asio::connect(socket, results.begin(), results.end());
+        boost::asio::connect(fastapi_socket, results.begin(), results.end());
 
         // Create the HTTP GET request
-        // Send username as part of the URL path(table_name)
         std::string url = "/check-users/" + username;  // use username as table_name
         http::request<http::string_body> req{ http::verb::get, url, 11 };
         req.set(http::field::host, "localhost");
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
         // Send the request to the server
-        http::write(socket, req);
+        http::write(fastapi_socket, req);
 
         // Prepare to read the response
         beast::flat_buffer buffer;
         http::response<http::string_body> res;
 
         // Receive the response
-        http::read(socket, buffer, res);
+        http::read(fastapi_socket, buffer, res);
 
         // Output the response
-        std::cout << "Response from FastAPI: " << res << std::endl;
+        //std::cout << "Response from FastAPI: " << res << std::endl;
+
+        // Parse the FastAPI JSON response
+        std::string fastapi_response = res.body();
+        boost::json::value json = boost::json::parse(fastapi_response);
+
+        // Form the response to be sent to the client (echoing back the JSON data)
+        std::string response_to_client = "Server: " + fastapi_response + "\n";
+
+        // Send the FastAPI response back to the client
+        //boost::asio::write(fastapi_socket, boost::asio::buffer(response_to_client));
 
         // Gracefully close the connection
-        socket.close();
+        fastapi_socket.close();
+
+        bool exists = json.at("exists").as_bool();
+        return exists;
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
+        return false;
     }
 }
 
 int main() {
-    std::string username = "user123"; // This could be received from the client
-    send_signal_to_fastapi(username);
-
     boost::asio::io_context io;
     tcp::acceptor acceptor(io, tcp::endpoint(boost::asio::ip::make_address("127.0.0.1"), 1234));
 
@@ -87,22 +97,34 @@ int main() {
 
         {
             std::lock_guard<std::mutex> lock(clients_mutex);
-            clients.push_back(socket);
+            clients.push_back(socket); 
+            std::cout << "Client " << client_id << " trying to connect\n";
         }
 
         // Start a new thread to handle the connected client
         std::thread([socket, client_id]() mutable {
             try {
                 std::string username;
-                boost::asio::read_until(*socket, boost::asio::dynamic_buffer(username), '\n');
-                username = username.substr(0, username.find('\n'));
-                std::string welcome = "Welcome, " + username + "\n";
-                boost::asio::write(*socket, boost::asio::buffer(welcome));
+                boost::asio::streambuf buf;
+                boost::asio::read_until(*socket, buf, '\n');
+                std::istream is(&buf);
+                std::getline(is, username);
 
                 client_names[socket] = username;
 
                 // Send the username to FastAPI for table existence check
-                send_signal_to_fastapi(username);
+                std::string log_message;
+
+                bool valid_username_check = send_signal_to_fastapi(username);
+                if (valid_username_check == true)
+                {
+                    log_message = "Welcome, " + username + "\n";
+                }
+                else
+                {
+                    log_message = "Username " + username + " does not exist\n";
+                }
+                boost::asio::write(*socket, boost::asio::buffer(log_message));
 
                 char data[128];
                 while (true) {
@@ -114,13 +136,24 @@ int main() {
                     broadcast(msg, socket);
                 }
             }
-            catch (...) {}
+            catch (const std::exception& e) {
+                std::cerr << "Exception in client thread: " << e.what() << "\n";
+            }
+            catch (...) {
+                std::cerr << "Unknown exception in client thread\n";
+            }
 
             // Remove this client from the list on disconnect
             std::lock_guard<std::mutex> lock(clients_mutex);
             clients.erase(std::remove(clients.begin(), clients.end(), socket), clients.end());
             client_names.erase(socket);
-            std::cout << "Client " << client_id << " disconnected\n";
+            auto it = client_names.find(socket);
+            if (it != client_names.end()) {
+                std::cout << "Client " << client_id << " (" << it->second << ") disconnected\n";
+            }
+            else {
+                std::cout << "Client " << client_id << " disconnected\n";
+            }
         }).detach();
     }
 }
